39286718cfca99dc11d306d44513e88c3123ca60
Duooongg <thuyduong10112005@gmail.com>
2024-08-04 18:32:05 +0700

    Add files via upload

---
+++ bai3buoi4.py
@@ -0,0 +1,18 @@
+set_A = {1,15,18,21,23}
+set_B = {6,66,78}
+
+point_happy = 0
+
+my_set = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+
+happy = my_set.intersection(set_A)
+unhappy = my_set.intersection(set_B)
+
+point_happy = point_happy + (len(happy) - len(unhappy))
+print("mức độ happy: ", point_happy)
+if point_happy > 0:
+    print("happy")
+elif point_happy == 0:
+    print("normal")
+else:
+    print("unhappy")
