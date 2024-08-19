775e738826d33f477cc4ea4814a9955898fa634c
Duooongg <thuyduong10112005@gmail.com>
2024-08-19 18:06:23 +0700

    Add files via upload

--- buoi6bai4.py
+++ buoi6bai4.py
@@ -14,11 +14,3 @@ def sum_number(number:int):
     return total
 print(len_number(12345))
 print(sum_number(12345))
-#5
-def format_phone_number(phone_number):
-    digits =''.join(filter(str.isdigit,phone_number))
-
-    if len(digits) !=10 or not digits.startswith('0'):
-        return "Ivalid phone number"
-    return "+84" + digits[1:]
-print(format_phone_number("0923-432-654"))
